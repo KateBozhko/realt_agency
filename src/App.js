4c3e8aee5c38862b570dcc4f3da5bf89b02b0c6a
Kate <bozhko2906872020@gmail.com>
2023-02-19 16:10:46 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import {data} from "./data";
+import Shop from "./Shop";
+import Contacts from "./Contacts";
+import About from "./About";
+import image from "./logo.jpg"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+    
+<Router>
+<nav>
+<Link to="/" className="link">Shop</Link>
+<Link to="/about" className="link">About us</Link>
+<Link to="/contacts" className="link">Our contacts</Link>
+</nav>
+<Routes>
+<Route path="/" element={<Shop/>}/>
+<Route path="/about" element={<About/>}/>
+<Route path="/contacts" element={<Contacts/>}/>
+</Routes>
+</Router>
+
   );
 }
-
 export default App;
---
+++ src/data.js
@@ -0,0 +1,31 @@
+export const data = [
+{
+id: 1,
+searchTerm: "studio",
+name: "Studio apartment",
+adress: "1292 ALBANY AVE BROOKLYN NY 11203-5638 USA",
+price: 320000,
+description: "One bathroom apartment, close to schools, shopping centres and park, has a new furniture. Furnished, some new windows, new carpet. For further information contact an agent: Paolo Garsia. Phone: +1 234 567-89-00",
+image: ["https://img.freepik.com/free-photo/3d-rendering-loft-luxury-living-room-with-bookshelf_105762-2104.jpg?w=1060&t=st=1673522496~exp=1673523096~hmac=29ec89d426d9d79f7cc0e3791093165fded5790d187a45d17fd0676aae6c2a31", "https://img.freepik.com/free-photo/room-interior-design_23-2148899461.jpg?w=996&t=st=1673522557~exp=1673523157~hmac=b461cb694a5170eb0889eb3c4ec403da6ddab91a81b09660b7520b817fa5f324"],
+},
+
+{
+    id: 2,
+    searchTerm: "two",
+    name: "Two-room apartment",
+    adress: "1159 GESELL RD EAST MEREDITH NY 13757-1111 USA",
+    price: 480000,
+    description: "Two-room apartmen, close to shopping mall, schools, park, has a new furniture. A perfect place for one  For further information contact an agent: Paolo Garsia. Phone: +1 234 567-89-00",
+    image: ["https://img.freepik.com/free-photo/3d-rendering-luxury-modern-bedroom-suite-tv-with-wardrobe-walk-closet_105762-1941.jpg?w=1380&t=st=1673522598~exp=1673523198~hmac=a667f38a1627cf2635c99327d28889305f81562c1066b1ebd60593dba9e1bdc8", "https://www.inmyroom.ru/photos/show/7458342", "https://www.inmyroom.ru/photos/show/7458358", "https://www.inmyroom.ru/photos/show/7458350", "https://www.inmyroom.ru/photos/show/7458353"],
+    },
+
+{
+    id: 3,
+    searchTerm: "three",
+    name: "Three-room apartment",
+    adress: "6000 HILLCROFT DR BOSTON NY 14025-9688 USA",
+    price: 510000,
+    description: "Three-room apartment, close to Cenrtal Park, coffee house, kindergarten shopping centres, has a new furniture. For further information contact an agent: Nick Greenwood. Phone: +1 234 387-21-35",
+    image: ["https://img.freepik.com/free-photo/3d-rendering-dining-room-kitchen-with-luxury-decor_105762-2056.jpg?w=1380&t=st=1673522636~exp=1673523236~hmac=e84a21df2a35db67c4b1dd19fa322d8192522c91f243abc7f76398ccb64a0288", "https://www.inmyroom.ru/photos/show/7487600", "https://www.inmyroom.ru/photos/show/7487593", "https://www.inmyroom.ru/photos/show/7487608", "https://www.inmyroom.ru/photos/show/7515173", "https://www.inmyroom.ru/photos/show/7487596"],
+    }
+]
