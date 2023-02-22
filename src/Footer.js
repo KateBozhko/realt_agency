5b10c3ae5038b7d0f5498809a33f30f78b035ad7
Kate <bozhko2906872020@gmail.com>
2023-02-22 11:24:20 +0300

    footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,6 +4,19 @@ function Footer() {
 <div className="video-wrapper">
 <iframe width="100%" height="500" src="https://www.youtube.com/embed/kroO5VE7X3w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 </div>
+<div className="wrapper-footer">
+
+<div className="logo"> 
+<img src="https://img.freepik.com/free-vector/house-logo-icon-negative-space-style_126523-692.jpg?w=1060&t=st=1677052975~exp=1677053575~hmac=60b6915a3dc13b4243ef981d75ac105f4dbf29c4bd2b3387105be49916662b2e" width="90px" alt="logo"/>
+</div>
+<div className="logo-item">
+<h1 className="footer-item">YOUR HOUSE, Inc.</h1>
+</div>
+</div>
+
+
+
+
         </div>
     )
 }
